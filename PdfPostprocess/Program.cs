c2bb5e19e2e165a74ea921322307f4ddcffd113b
serge <serge.sotnyk@gmail.com>
2019-06-18 14:50:25 +0300

    Initial project structure

---
+++ PdfPostprocess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PdfPostprocess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
