65a776472684051649c1c19c96f7d95d6ddc1a8b
serge <serge.sotnyk@gmail.com>
2019-06-18 16:50:46 +0300

    not finished...

---
+++ PdfPostprocess/ConsoleHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfPostprocess
+{
+    public static class ConsoleHelper
+    {
+        public static void ConsolePressAnyKey()
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PdfPostprocess/CorrectionData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfPostprocess
+{
+    public class CorrectionData
+    {
+        public string LineText { get; set; }
+        public bool GlueWithPrevious { get; set; }
+    }
+}
